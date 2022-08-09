40bfee4b85675be65dddb94eb4218e03dc15691b
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-08-09 17:14:10 +0300

    did task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+  refs.btnStop.removeAttribute('disabled');
+  refs.btnStart.setAttribute('disabled', 'disabled');
+  refs.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStop() {
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+}
