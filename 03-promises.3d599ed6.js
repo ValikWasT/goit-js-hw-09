bd206305246878e4d6f425521fcfe7898abcccbf
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-08-09 18:21:20 +0000

    Deploying to gh-pages from @ ValikWasT/goit-js-hw-09@9a29a1bdc573b939f8bf11a601c1fea87a159237 🚀

---
+++ 03-promises.3d599ed6.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]'),document.querySelector("form > button");
+//# sourceMappingURL=03-promises.3d599ed6.js.map
