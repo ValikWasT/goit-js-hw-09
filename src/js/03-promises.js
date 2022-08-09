9a29a1bdc573b939f8bf11a601c1fea87a159237
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-08-09 21:20:43 +0300

    to do task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const refs = {
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+  submitBtn: document.querySelector('form > button'),
+};
+// refs.submitBtn.addEventListener(
+//   'submit',
+//   createPromise(refs.inputAmount.value, refs.inputDelay.value)
+// );
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  stopDefAction();
+  for (let i = 0; i < position; i += 1) {
+    const shouldResolve = Math.random() > 0.3;
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve('Success! Value passed to resolve function');
+        } else {
+          reject('Error! Error passed to reject function');
+        }
+      }, delay);
+    });
+    promise
+      .then(value => {
+        console.log(value);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    delay += 500;
   }
 }
