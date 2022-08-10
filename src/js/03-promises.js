bd4e1b9cb93597c79b43d335300d09186834543a
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-08-10 13:17:56 +0300

    did task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,33 +1,45 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   inputDelay: document.querySelector('input[name="delay"]'),
   inputStep: document.querySelector('input[name="step"]'),
   inputAmount: document.querySelector('input[name="amount"]'),
   form: document.querySelector('form'),
 };
+
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
-  createPromise(refs.inputAmount.value, refs.inputDelay.value);
+  if (
+    Number.parseInt(refs.inputDelay.value) < 0 ||
+    Number.parseInt(refs.inputAmount.value) < 0 ||
+    Number.parseInt(refs.inputStep.value) < 0
+  ) {
+    Notiflix.Notify.failure('Error! Your values is negative!');
+    return;
+  }
+  let delayValue = Number.parseInt(refs.inputDelay.value);
+  for (let i = 1; i <= Number.parseInt(refs.inputAmount.value); i += 1) {
+    createPromise(Number.parseInt(i), delayValue);
+    delayValue += Number.parseInt(refs.inputStep.value);
+  }
 });
 
 function createPromise(position, delay) {
-  for (let i = 0; i < position; i += 1) {
-    const shouldResolve = Math.random() > 0.3;
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve('Success! Value passed to resolve function');
-        } else {
-          reject('Error! Error passed to reject function');
-        }
-      }, delay);
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
-    promise
-      .then(value => {
-        console.log(value);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    delay += 500;
-  }
 }
